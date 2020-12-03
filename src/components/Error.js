75c45b2f6ba1c341da4daef8c9ee4150c12e6ec6
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-03 11:42:12 +0530

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,8 @@
 import React, { Component, useState } from "react";
-import { Router, Route, Switch } from "react-router-dom";
 import "../styles/App.css";
 
 export default class Error extends Component {
   render() {
-    return <>No Match</>;
+    return <div>No Match</div>;
   }
 }
