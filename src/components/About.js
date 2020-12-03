a09632536ec103bb828dc0177fd0ddc7fc9e7e51
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-12-03 11:43:24 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import React, { Component, useState } from "react";
-import { Router, Route, Switch } from "react-router-dom";
 import "../styles/App.css";
 
 export default class About extends Component {
